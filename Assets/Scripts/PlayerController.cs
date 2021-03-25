a4cca981b8363f14bee27ea95a3dbeef70e3e90e
Sarah Ottinger <schalaalexiazeal@gmail.com>
2021-03-25 12:51:55 -0700

    Damage mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,51 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Capstone
 {
 	/// <summary>
-	/// Various states of player input
+	/// Various states of our player
 	/// </summary>
 	[Flags]
-	public enum PlayerInputFlags : byte
+	public enum PlayerState : byte
 	{
 		None = 0,
 		Grounded = 1,
 		Moving = 2,
 		Jumped = 4,
-		Fired = 8
+		Fired = 8,
+		Damaged = 16
 	}
 
 	/// <summary>
 	/// Manages player input, movement and actions
 	/// </summary>
 	[RequireComponent(typeof(CharacterController))]
+	[RequireComponent(typeof(CharacterRigidbody))]
+	[RequireComponent(typeof(Material))]
 	public class PlayerController : MonoBehaviour, InputActions.IPlayerActions
 	{
 		[SerializeField] float moveSpeed = 5f;
 		[SerializeField] float jumpForce = 1f;
-		[SerializeField] float jumpTimeMax = 0.25f;
+		[SerializeField] float jumpTimeFrame = 0.25f;
 		[SerializeField] float gravityMultiplier = -3f;
+		[SerializeField] float forceWhenDamaged = 5f;
+		[SerializeField] float limitedInvulnerabilityTime = 1f;
 		[SerializeField] int maxJumps = 2;
 
 		CharacterController controller = null;
+		CharacterRigidbody physicsBody = null;
+		Material playerMaterial = null;
 		InputActions inputActions = null;
+		Color materialColor;
 		Vector3 velocity = Vector3.zero;
 		float movement = 0f;
-		float jumpDelta = 0f;
+		float groundDelta = 0f;
+		float damagedDelta = 0f;
 		int jumpCount = 0;
-		PlayerInputFlags flags = PlayerInputFlags.None;
+		PlayerState state = PlayerState.None;
 
 		/// <summary>
 		/// Set up our input actions.
@@ -43,6 +53,9 @@ namespace Capstone
 		private void Awake()
 		{
 			controller = GetComponent<CharacterController>();
+			physicsBody = GetComponent<CharacterRigidbody>();
+			playerMaterial = GetComponentInChildren<Renderer>().material;
+			materialColor = playerMaterial.color;
 
 			inputActions = new InputActions();
 			inputActions.Player.SetCallbacks(this);
@@ -55,15 +68,22 @@ namespace Capstone
 		{
 			velocity = controller.velocity;
 
-			if (flags.HasFlag(PlayerInputFlags.Moving))
+			if (state.HasFlag(PlayerState.Moving))
 				controller.Move(Vector3.forward * movement * moveSpeed * Time.deltaTime);
 
-			if (!flags.HasFlag(PlayerInputFlags.Grounded))
+			if (!state.HasFlag(PlayerState.Grounded))
 				velocity.y += Physics.gravity.y * Time.deltaTime;
 			controller.Move(velocity * Time.deltaTime); // apply gravity
 
-			if (flags.HasFlag(PlayerInputFlags.Jumped))
-				jumpDelta += Time.deltaTime;
+			if (state.HasFlag(PlayerState.Jumped))
+				groundDelta += Time.deltaTime;
+
+			if (state.HasFlag(PlayerState.Damaged))
+			{
+				damagedDelta -= Time.deltaTime;
+				if (damagedDelta <= 0f)
+					state &= ~PlayerState.Damaged;
+			}
 		}
 
 		/// <summary>
@@ -82,12 +102,41 @@ namespace Capstone
 		/// <param name="hit">What was hit</param>
 		private void OnControllerColliderHit(ControllerColliderHit hit)
 		{
-			if (hit.gameObject.CompareTag("Ground"))
+			if (hit.gameObject.CompareTag("Ground")) // reset jump conditions
 			{
-				flags |= PlayerInputFlags.Grounded;
-				flags &= ~PlayerInputFlags.Jumped;
+				state |= PlayerState.Grounded;
+				state &= ~PlayerState.Jumped;
 				jumpCount = 0;
-				jumpDelta = 0f;
+				groundDelta = 0f;
+			}
+			else
+				state &= ~PlayerState.Grounded;
+
+			// give the player some knockback and activate limited invulnerability
+			if (hit.gameObject.CompareTag("Enemy") || hit.gameObject.CompareTag("Hazard"))
+			{
+				if (!state.HasFlag(PlayerState.Damaged))
+				{
+					physicsBody.AddImpact(Vector3.Normalize(-transform.forward), forceWhenDamaged);
+					state |= PlayerState.Damaged;
+					state &= ~PlayerState.Grounded;
+					damagedDelta = limitedInvulnerabilityTime;
+					StartCoroutine(DamageEffect());
+				}
+			}
+		}
+
+		IEnumerator DamageEffect()
+		{
+			while (damagedDelta > 0f)
+			{
+				materialColor.a = 0f;
+				playerMaterial.color = materialColor;
+				yield return new WaitForSeconds(0.1f);
+
+				materialColor.a = 1f;
+				playerMaterial.color = materialColor;
+				yield return new WaitForSeconds(0.1f);
 			}
 		}
 
@@ -105,17 +154,17 @@ namespace Capstone
 		{
 			if (context.started)
 			{
-				if (flags.HasFlag(PlayerInputFlags.Jumped) && jumpCount < maxJumps)
+				if (state.HasFlag(PlayerState.Jumped) && jumpCount < maxJumps)
 				{
 					controller.velocity.Set(controller.velocity.x, 0f, controller.velocity.z); // zero out velocity for double (triple?) jumps
 					Jump();
 				}
 
-				if (flags.HasFlag(PlayerInputFlags.Grounded))
+				if (state.HasFlag(PlayerState.Grounded))
 				{
 					Jump();
-					flags |= PlayerInputFlags.Jumped;
-					flags &= ~PlayerInputFlags.Grounded;
+					state |= PlayerState.Jumped;
+					state &= ~PlayerState.Grounded;
 				}
 
 				jumpCount++;
@@ -136,11 +185,11 @@ namespace Capstone
 		/// </summary>
 		public void OnMove(InputAction.CallbackContext context)
 		{
-			if (context.started) flags |= PlayerInputFlags.Moving;
-			if (context.canceled) flags &= ~PlayerInputFlags.Moving;
+			if (context.started) state |= PlayerState.Moving;
+			if (context.canceled) state &= ~PlayerState.Moving;
 
 			var input = context.ReadValue<Vector2>();
-			movement = -input.x; // Negate input or our movement will be inverted.
+			movement = input.x;
 		}
 	}
 }
