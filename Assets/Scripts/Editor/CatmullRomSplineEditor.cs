f7793c82a1e477d65c1e1e3f5d7a87c60eeff23d
Sarah Ottinger <schalaalexiazeal@gmail.com>
2021-04-27 14:46:11 -0700

    splines almost done

--- Assets/Scripts/Editor/CatmullRomSplineEditor.cs
+++ Assets/Scripts/Editor/CatmullRomSplineEditor.cs
@@ -9,7 +9,12 @@ namespace Capstone.Helpers
 	[CustomEditor(typeof(CatmullRomSpline))]
 	public class CatmullRomSplineEditor : Editor
 	{
-		GUIStyle style = new GUIStyle();
+		const float demoLineWidth = 2.5f;
+
+		GUIStyle style1 = new GUIStyle();
+		GUIStyle style2 = new GUIStyle();
+		float demoT = 0f;
+		float radius = 10f;
 
 		/// <summary>
 		/// Construct the property view.
@@ -60,6 +65,30 @@ namespace Capstone.Helpers
 			}
 			EditorGUILayout.EndHorizontal();
 
+			// circle
+			if (spline.looped)
+			{
+				EditorGUILayout.BeginHorizontal();
+				GUILayout.Label("Radius: ");
+				radius = float.Parse(GUILayout.TextField(radius.ToString()));
+				if (GUILayout.Button("Convert to Circle", EditorStyles.miniButtonRight))
+				{
+					Undo.RecordObject(spline, "Convert to Circle");
+					spline.Circle(radius);
+					serializedObject.Update();
+				}
+				EditorGUILayout.EndHorizontal();
+			}
+
+			// This is to just demo the forward vector of points in the scene view of the editor for debug purposes.
+			if (spline.looped)
+			{
+				EditorGUILayout.BeginHorizontal();
+				GUILayout.Label("Preview Agent: ");
+				demoT = GUILayout.HorizontalSlider(demoT, 0f, spline.points.Count - 0.001f);
+				EditorGUILayout.EndHorizontal();
+			}
+
 			serializedObject.ApplyModifiedProperties();
 		}
 
@@ -68,10 +97,13 @@ namespace Capstone.Helpers
 		/// </summary>
 		public void OnSceneGUI()
 		{
-			style.normal.textColor = Color.green;
-			style.fontSize = 24;
+			style1.normal.textColor = Color.green;
+			style1.fontSize = 24;
 
-			Tools.current = Tool.None;
+			style2.normal.textColor = Color.blue;
+			style2.fontSize = 24;
+
+			Tools.current = Tool.None; // disable default handles
 
 			var spline = (CatmullRomSpline)target;
 			var points = spline.points;
@@ -89,29 +121,38 @@ namespace Capstone.Helpers
 				var deltaPos = newSplinePos - spline.transform.position;
 				spline.transform.position = newSplinePos;
 				for (int i = 0; i < points.Count; i++)
-					points[i] += deltaPos;
+				{
+					var p = points[i];
+					p.Position += deltaPos;
+					points[i] = p;
+				}
 				serializedObject.Update();
 			}
 
 			// and for each point
 			for (int i = 0; i < points.Count; i++)
 			{
-				Handles.color = Color.blue;
-				Handles.SphereHandleCap(0, points[i], Quaternion.identity, 0.5f, EventType.Repaint);
+				var p = points[i];
+				/*if (spline.looped) */p.Length = spline.GetLength(i);
 
-				// label our points with a number
+				Handles.color = Color.blue;
+				Handles.SphereHandleCap(0, p.Position, Quaternion.identity, 0.5f, EventType.Repaint);
+
+				// label our points with a number, length, and a location hint
+				Handles.Label(p.Position, $"X: {p.Position.x}, Y: {p.Position.y}, Z: {p.Position.z}");
+				Handles.Label(p.Position + Vector3.down * 0.5f, $"Len: {p.Length}");
 				Handles.BeginGUI();
-				var pos2D = HandleUtility.WorldToGUIPoint(points[i]);
-				GUI.Label(new Rect(pos2D.x, pos2D.y, 50f, 50f), i.ToString(), style);
+				var pos2D = HandleUtility.WorldToGUIPoint(p.Position);
+				GUI.Label(new Rect(pos2D.x, pos2D.y, 50f, 50f), i.ToString(), style1);
 				Handles.EndGUI();
 
 				// handle movement of a point
 				EditorGUI.BeginChangeCheck();
-				var newPosition = Handles.PositionHandle(points[i], Quaternion.identity);
+				var newPosition = Handles.PositionHandle(p.Position, Quaternion.identity);
 				if (EditorGUI.EndChangeCheck())
 				{
 					Undo.RecordObject(spline, "Change Point Position");
-					points[i] = newPosition;
+					p.Position = newPosition;
 					serializedObject.Update();
 				}
 			}
@@ -122,26 +163,47 @@ namespace Capstone.Helpers
 				// draw circle
 				for (float t = 0.005f; t < points.Count; t += 0.005f)
 				{
-					var prev = spline.GetPoint(t - 0.005f);
-					var pos = spline.GetPoint(t);
+					var prev = spline.GetPoint(t - 0.005f).Position;
+					var pos = spline.GetPoint(t).Position;
 					Handles.DrawLine(prev, pos);
 				}
 			}
 			else
 			{
+				demoT /= spline.points.Count; // normalise agent to avoid out of range
+
 				// draw curve
 				for (float t = 0.005f; t < points.Count - 3f; t += 0.005f)
 				{
-					var prev = spline.GetPoint(t - 0.005f);
-					var pos = spline.GetPoint(t);
+					var prev = spline.GetPoint(t - 0.005f).Position;
+					var pos = spline.GetPoint(t).Position;
 					Handles.DrawLine(prev, pos);
 				}
 
 				// draw a dotted line for the anchors
 				Handles.color = Color.green;
-				Handles.DrawDottedLine(points[0], points[1], 3f);
-				Handles.DrawDottedLine(points[points.Count - 1], points[points.Count - 2], 3f);
+				Handles.DrawDottedLine(points[0].Position, points[1].Position, 3f);
+				Handles.DrawDottedLine(points[points.Count - 1].Position, points[points.Count - 2].Position, 3f);
 			}
+
+			// draw our demo agent for forward previewing
+			Handles.color = Color.red;
+			var demoPoint = spline.GetPoint(demoT);
+			var demoForward = spline.GetPoint(demoT, true);
+			float r = Mathf.Atan2(-demoForward.Position.z, demoForward.Position.x);
+			var demoP1 = new Vector3
+			{
+				x = demoLineWidth * Mathf.Sin(r) + demoPoint.Position.x,
+				y = demoPoint.Position.y,
+				z = demoLineWidth * Mathf.Cos(r) + demoPoint.Position.z,
+			};
+			var demoP2 = new Vector3
+			{
+				x = -demoLineWidth * Mathf.Sin(r) + demoPoint.Position.x,
+				y = demoPoint.Position.y,
+				z = -demoLineWidth * Mathf.Cos(r) + demoPoint.Position.z,
+			};
+			Handles.DrawLine(demoP1, demoP2);
 		}
 	}
 }
--- Assets/Scripts/Helpers/CatmullRomSpline.cs
+++ Assets/Scripts/Helpers/CatmullRomSpline.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,13 +12,26 @@ namespace Capstone.Helpers
 		Z
 	}
 
+	[Serializable]
+	public class SplinePoint
+	{
+		public Vector3 Position;
+		public float Length;
+
+		public SplinePoint(Vector3 pos)
+		{
+			Position = pos;
+			Length = 0f;
+		}
+	}
+
 	/// <summary>
 	/// Implementation of a 3D Catmull-Rom spline
 	/// </summary>
 	public class CatmullRomSpline : MonoBehaviour
 	{
 		public bool looped = false;
-		public List<Vector3> points;
+		public List<SplinePoint> points;
 		public Transform startTrigger = null;
 		public Transform endTrigger = null;
 
@@ -28,18 +42,18 @@ namespace Capstone.Helpers
 		{
 			if (points == null)
 			{
-				points = new List<Vector3>
+				points = new List<SplinePoint>
 				{
-					transform.position + Vector3.right,
-					transform.position + new Vector3(2f, 0f, 0f),
-					transform.position + new Vector3(3f, 0f, 0f),
-					transform.position + new Vector3(4f, 0f, 0f)
+					new SplinePoint(transform.position + Vector3.right),
+					new SplinePoint(transform.position + new Vector3(2f, 0f, 0f)),
+					new SplinePoint(transform.position + new Vector3(3f, 0f, 0f)),
+					new SplinePoint(transform.position + new Vector3(4f, 0f, 0f))
 				};
 			}
 			else
 			{
-				var lastPoint = points[points.Count - 1];
-				points.Add(lastPoint + Vector3.right);
+				var lastPosition = points[points.Count - 1].Position;
+				points.Add(new SplinePoint(lastPosition + Vector3.right));
 			}
 		}
 
@@ -53,9 +67,9 @@ namespace Capstone.Helpers
 		}
 
 		/// <summary>
-		/// Get the point along the spline, relative to `t`.
+		/// Get the point along the spline, or alternatively the forward vector, relative to `t`.
 		/// </summary>
-		public Vector3 GetPoint(float t)
+		public SplinePoint GetPoint(float t, bool getForward = false)
 		{
 			int p0, p1, p2, p3;
 
@@ -78,16 +92,27 @@ namespace Capstone.Helpers
 			float tt = t * t;
 			float ttt = tt * t;
 
-			float q0 = -ttt + 2f * tt - t;
-			float q1 = 3f * ttt - 5f * tt + 2f;
-			float q2 = -3f * ttt + 4f * tt + t;
-			float q3 = ttt - tt;
+			float q0, q1, q2, q3;
+			if (getForward)
+			{
+				q0 = -3f * tt + 4f * t - 1f;
+				q1 = 9f * tt - 10f * t;
+				q2 = -9f * tt + 8f * t + 1f;
+				q3 = 3f * tt - 2f * t;
+			}
+			else
+			{
+				q0 = -ttt + 2f * tt - t;
+				q1 = 3f * ttt - 5f * tt + 2f;
+				q2 = -3f * ttt + 4f * tt + t;
+				q3 = ttt - tt;
+			}
 
-			float tx = 0.5f * (points[p0].x * q0 + points[p1].x * q1 + points[p2].x * q2 + points[p3].x * q3);
-			float ty = 0.5f * (points[p0].y * q0 + points[p1].y * q1 + points[p2].y * q2 + points[p3].y * q3);
-			float tz = 0.5f * (points[p0].z * q0 + points[p1].z * q1 + points[p2].z * q2 + points[p3].z * q3);
+			float tx = 0.5f * (points[p0].Position.x * q0 + points[p1].Position.x * q1 + points[p2].Position.x * q2 + points[p3].Position.x * q3);
+			float ty = 0.5f * (points[p0].Position.y * q0 + points[p1].Position.y * q1 + points[p2].Position.y * q2 + points[p3].Position.y * q3);
+			float tz = 0.5f * (points[p0].Position.z * q0 + points[p1].Position.z * q1 + points[p2].Position.z * q2 + points[p3].Position.z * q3);
 
-			return new Vector3(tx, ty, tz);
+			return new SplinePoint(new Vector3(tx, ty, tz));
 		}
 
 		/// <summary>
@@ -100,9 +125,9 @@ namespace Capstone.Helpers
 			// grab the axis of the first point
 			switch (axis)
 			{
-				case Axis.X: lowest = points[0].x; break;
-				case Axis.Y: lowest = points[0].y; break;
-				case Axis.Z: lowest = points[0].z; break;
+				case Axis.X: lowest = points[0].Position.x; break;
+				case Axis.Y: lowest = points[0].Position.y; break;
+				case Axis.Z: lowest = points[0].Position.z; break;
 			}
 
 			// compare to see if subsequent points have a lower value
@@ -111,13 +136,13 @@ namespace Capstone.Helpers
 				switch (axis)
 				{
 					case Axis.X:
-						if (points[i].x < lowest) lowest = points[i].x;
+						if (points[i].Position.x < lowest) lowest = points[i].Position.x;
 						break;
 					case Axis.Y:
-						if (points[i].y < lowest) lowest = points[i].z;
+						if (points[i].Position.y < lowest) lowest = points[i].Position.z;
 						break;
 					case Axis.Z:
-						if (points[i].z < lowest) lowest = points[i].z;
+						if (points[i].Position.z < lowest) lowest = points[i].Position.z;
 						break;
 				}
 			}
@@ -128,12 +153,56 @@ namespace Capstone.Helpers
 				var point = points[i];
 				switch (axis)
 				{
-					case Axis.X: point.x = lowest; break;
-					case Axis.Y: point.y = lowest; break;
-					case Axis.Z: point.z = lowest; break;
+					case Axis.X: point.Position.x = lowest; break;
+					case Axis.Y: point.Position.y = lowest; break;
+					case Axis.Z: point.Position.z = lowest; break;
 				}
 				points[i] = point;
 			}
 		}
+
+		/// <summary>
+		/// Reshape the spline into a circle of the specified radius.
+		/// </summary>
+		public void Circle(float radius)
+		{
+			if (!looped) return;
+
+			for (int i = 0; i < points.Count; i++)
+				points[i] = new SplinePoint(new Vector3
+				{
+					x = radius * Mathf.Sin((float)i / points.Count * Mathf.PI * 2f),
+					y = points[i].Position.y,
+					z = radius * Mathf.Cos((float)i / points.Count * Mathf.PI * 2f)
+				});
+		}
+
+		/// <summary>
+		/// Get the length approximation of a segment, given its index in the list.
+		/// </summary>
+		public float GetLength(int point)
+		{
+			if (point > points.Count) return 0f;
+
+			float length = 0f;
+			var prev = GetPoint(point);
+			SplinePoint newPoint;
+
+			for (float t = 0f; t < 1f; t += 0.005f)
+			{
+				newPoint = GetPoint(point + t);
+				length = Mathf.Sqrt(Mathf.Pow(newPoint.Position.x - prev.Position.x, 2) + Mathf.Pow(newPoint.Position.y - prev.Position.y, 2) +
+					Mathf.Pow(newPoint.Position.z - prev.Position.z, 2));
+				prev = newPoint;
+			}
+
+			return length;
+		}
+
+		/*public float GetNormalisedOffset(float p)
+		{
+			int i = 0;
+
+		}*/
 	}
 }
